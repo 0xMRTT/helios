89ea535d1759e3a5a680aab5bd1ff331cb27c5a8
0xMRTT <0xMRTT@tuta.io>
2022-06-17 10:57:54 +0200

    Add libs for temperature

--- helios.ino
+++ helios.ino
@@ -1,3 +1,13 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+const int SENSOR_PIN = 13; /
+
+OneWire oneWire(SENSOR_PIN);         
+DallasTemperature tempSensor(&oneWire); 
+
+float tempCelsius;    
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Initializing");
